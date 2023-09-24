e3a6eca456cfe7888c9914d0cf7dc1621b5d7fd0
Gsus <jago2811@gmail.com>
2023-09-24 13:11:29 -0600

    [start]

---
+++ main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	screenWidth  = 1000
+	screenHeight = 500
+)
+
+var (
+	running  = true
+	bkgColor = rl.NewColor(147, 211, 196, 255)
+
+	grassSprite  = rl.Texture2D{}
+	playerSprite = rl.Texture2D{}
+
+	playerSrc                                     rl.Rectangle
+	playerDest                                    rl.Rectangle
+	playerMoving                                  bool
+	playerDir                                     int
+	playerUp, playerDown, playerRight, playerLeft bool
+
+	playerFrame int
+	frameCount  int
+
+	tileDest   rl.Rectangle
+	tileSrc    rl.Rectangle
+	tileMap    []int
+	srcMap     []string
+	mapW, mapH int
+
+	playerSpeed float32 = 3
+
+	cam rl.Camera2D
+)
+
+func drawScene() {
+	//rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	for i := 0; i < len(tileMap); i++ {
+		if tileMap[i] != 0 {
+			tileDest.X = tileDest.Width * float32(i%mapW)
+			tileDest.Y = tileDest.Height * float32(i/mapW)
+
+			tileSrc.X = tileSrc.Width * float32((tileMap[i]-1)%int(grassSprite.Width/int32(tileSrc.Width)))
+			tileSrc.Y = tileSrc.Height * float32((tileMap[i]-1)/int(grassSprite.Width/int32(tileSrc.Width)))
+
+			rl.DrawTexturePro(grassSprite, tileSrc, tileDest, rl.NewVector2(tileDest.Width, tileDest.Height), 0, rl.White)
+		}
+	}
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
+
+func input() {
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyComma) {
+		playerMoving = true
+		playerDir = 1
+		playerUp = true
+
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyO) {
+		playerMoving = true
+		playerDir = 0
+		playerDown = true
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyA) {
+		playerMoving = true
+		playerDir = 2
+		playerLeft = true
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyE) {
+		playerMoving = true
+		playerDir = 3
+		playerRight = true
+	}
+}
+
+func update() {
+	running = !rl.WindowShouldClose()
+
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+	if playerMoving {
+		if playerUp {
+			playerDest.Y -= playerSpeed
+		}
+		if playerDown {
+			playerDest.Y += playerSpeed
+		}
+		if playerLeft {
+			playerDest.X -= playerSpeed
+		}
+		if playerRight {
+			playerDest.X += playerSpeed
+		}
+		if frameCount%8 == 1 {
+			playerFrame++
+		}
+	} else if frameCount%45 == 1 {
+		playerFrame++
+
+	}
+	frameCount++
+	if playerFrame > 3 {
+		playerFrame = 0
+	}
+	if !playerMoving && playerFrame > 1 {
+		playerFrame = 0
+	}
+	playerSrc.X = playerSrc.Width * float32(playerFrame)
+
+	playerSrc.Y = playerSrc.Height * float32(playerDir)
+
+	cam.Target = rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2)))
+	playerMoving = false
+	playerUp, playerDown, playerRight, playerLeft = false, false, false, false
+
+}
+
+func render() {
+	rl.BeginDrawing()
+	rl.ClearBackground(bkgColor)
+
+	rl.BeginMode2D(cam)
+
+	drawScene()
+
+	rl.EndMode2D()
+	rl.EndDrawing()
+}
+
+func loadMap() {
+	mapW = 5
+	mapH = 5
+	for i := 0; i < (mapW * mapH); i++ {
+		tileMap = append(tileMap, 1)
+
+	}
+
+}
+
+func initializing() {
+	rl.InitWindow(screenWidth, screenHeight, "raylib [core] - concurrencia")
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(60)
+
+	grassSprite = rl.LoadTexture("src/assets/Sprout Lands - Sprites - Basic pack/Tilesets/Grass.png")
+
+	tileDest = rl.NewRectangle(0, 0, 16, 16)
+	tileSrc = rl.NewRectangle(0, 0, 16, 16)
+
+	playerSprite = rl.LoadTexture("src/assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png")
+
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+	cam = rl.NewCamera2D(rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
+		rl.NewVector2(float32(playerDest.X-(playerDest.Width/2)), float32(playerDest.Y-(playerDest.Height/2))), 0.0, 1.8)
+	loadMap()
+}
+
+func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
+	rl.CloseWindow()
+}
+
+func main() {
+	initializing()
+
+	for running {
+		input()
+		update()
+		render()
+	}
+	quit()
+}
